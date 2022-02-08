75713c810f78d8130b23569a7a5b6829af4ba1ae
Augusto Perez <augustoperez.dev@gmail.com>
2022-02-07 18:35:53 -0600

    'maquetado'

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Header'>
+
+      </div>
+      <div className='section1'>
+
+      </div>
+      <div className='section2'>
+
+      </div>
+      <div className='section3'>
+
+      </div>
+      <div className='section4'>
+
+      </div>
+      
+      <div className='section5'>
+
+      </div>
     </div>
   );
 }
